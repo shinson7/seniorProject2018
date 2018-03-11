d58df8d3a05b2c5ad460037a6257783d8a2d695a
elusch21 <eschreiber@eastsideprep.org>
2018-03-10 18:26:42 -0800

    I did a thing
    i did some stuff. InputProcessor added, base image added. Scaling is up
    next.

--- core/src/com/mygdx/game/LinkTrainer.java
+++ core/src/com/mygdx/game/LinkTrainer.java
@@ -2,26 +2,34 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 
-public class LinkTrainer extends ApplicationAdapter {
+public class LinkTrainer extends ApplicationAdapter implements InputProcessor {
 	SpriteBatch batch;
 	Texture img;
+        Texture turf;
+        Sprite player;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+                turf = new Texture("turf.jpg");
+                player = new Sprite(img);
+                player.setPosition(Gdx.graphics.getWidth()/2, 0);
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(turf, 0, 0);
+                player.draw(batch);
 		batch.end();
 	}
 	
@@ -30,4 +38,44 @@ public class LinkTrainer extends ApplicationAdapter {
 		batch.dispose();
 		img.dispose();
 	}
+
+    @Override
+    public boolean keyDown(int i) {
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int i) {
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char c) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int i, int i1, int i2, int i3) {
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int i, int i1, int i2, int i3) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int i, int i1, int i2) {
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int i, int i1) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(int i) {
+        return false;
+    }
 }
