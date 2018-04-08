086a4244cc69553f0de9facb503c70709d6536a9
shinson7 <shinson@eastsideprep.org>
2018-04-08 13:34:04 -0700

    Stuff

--- core/src/com/mygdx/game/LinkTrainer.java
+++ core/src/com/mygdx/game/LinkTrainer.java
@@ -30,7 +30,7 @@ public class LinkTrainer extends ApplicationAdapter implements InputProcessor {
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(0, 0, 1, 1);
+		Gdx.gl.glClearColor(.529f, .808f, .922f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		turfBack.draw(batch);
