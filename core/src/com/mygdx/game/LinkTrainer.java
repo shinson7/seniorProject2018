622f5fad4321dd1a3a4de2f499db368fba7cc2d7
shinson7 <shinson@eastsideprep.org>
2018-05-11 15:18:07 -0700

    Other text implemented
    overhauled boolean structure and text

--- core/src/com/mygdx/game/LinkTrainer.java
+++ core/src/com/mygdx/game/LinkTrainer.java
@@ -74,9 +74,13 @@ public class LinkTrainer extends ApplicationAdapter implements InputProcessor {
         //boolean chain to determine what gets shown
         if (currentScenario.initialExplainationPassed == false && prefaceDone) {
             //Present initial explanation
-            font.draw(batch, currentScenario.ie, 125, 100, 400, 10, true);
+            font.draw(batch, currentScenario.ie + " Press ENTER to continue.", 125, 100, 400, 10, true);
             font.getData().setScale(.75f);
-        } else if (currentScenario.initialVideoPassed == false && currentScenario.initialExplainationPassed && dn == false) {
+        } else if (currentScenario.videoPrefacePassed == false && currentScenario.initialExplainationPassed && dn == false) {
+            font.draw(batch, currentScenario.vp + " Press ENTER to play the video.", 125, 100, 400, 10, true);
+            font.getData().setScale(.75f);
+        } else if (currentScenario.initialVideoPassed == false && currentScenario.videoPrefacePassed && currentScenario.initialExplainationPassed && dn == false) {
+            System.out.println("arrived");
             //open video
             try {
                 Desktop.getDesktop().open(new File(currentScenario.iv)); //This opens the video
@@ -87,13 +91,18 @@ public class LinkTrainer extends ApplicationAdapter implements InputProcessor {
             currentScenario.initialVideoPassed = true;
         } else if (currentScenario.initialVideoPassed && currentScenario.choicePassed == false && dn == false) {
             //give our four options
-            font.draw(batch, currentScenario.o1, 50, 100, 100, 10, true);
-            font.draw(batch, currentScenario.o2, 200, 100, 100, 10, true);
-            font.draw(batch, currentScenario.o3, 350, 100, 100, 10, true);
-            font.draw(batch, currentScenario.o4, 500, 100, 100, 10, true);
+            font.draw(batch, currentScenario.o1 + " Press 1 to select this option.", 50, 100, 100, 10, true);
+            font.draw(batch, currentScenario.o2 + " Press 2 to select this option.", 200, 100, 100, 10, true);
+            font.draw(batch, currentScenario.o3 + " Press 3 to select this option.", 350, 100, 100, 10, true);
+            font.draw(batch, currentScenario.o4 + " Press 4 to select this option.", 500, 100, 100, 10, true);
             font.getData().setScale(.5f);
-        } else if (currentScenario.initialExplainationPassed && currentScenario.choicePassed && currentScenario.finalExplainationPassed == false && dn == false) {
-            font.draw(batch, currentScenario.fe, 125, 100, 400, 10, true);
+        } else if (currentScenario.initialExplainationPassed && currentScenario.choicePassed && currentScenario.finalExplaination1Passed == false && dn == false) {
+            //show fe1
+            font.draw(batch, currentScenario.fe1 + " Please press ENTER to find out why the other options were wrong.", 125, 100, 400, 10, true);
+            font.getData().setScale(.5f);
+        } else if (currentScenario.initialExplainationPassed && currentScenario.choicePassed && currentScenario.finalExplaination1Passed && currentScenario.finalExplaination2Passed == false && dn == false) {
+            //show fe2
+            font.draw(batch, currentScenario.fe2 + "Please press ENTER to watch the video of this being executed. When you have watched the video please press ENTER again to advance to the next scenario.", 125, 100, 400, 10, true);
             font.getData().setScale(.5f);
             //need to check for the video while still in our final explaination
             if (currentScenario.finalVideoPassed == false && showVideo && currentScenario.initialExplainationPassed && currentScenario.choicePassed && dn == false) {
@@ -165,15 +174,23 @@ public class LinkTrainer extends ApplicationAdapter implements InputProcessor {
                 }
                 break;
             case Keys.ENTER:
+                System.out.println("hi");
                 if (prefaceDone == false) {
                     prefaceDone = true;
                 } else if (currentScenario.initialExplainationPassed == false) {
                     //if you have just seen the initial explaination
                     currentScenario.initialExplainationPassed = true;
-                } else if (currentScenario.initialExplainationPassed && currentScenario.initialVideoPassed && currentScenario.choicePassed && currentScenario.finalVideoPassed == false) {
+                    System.out.println("ie");
+                } else if (currentScenario.videoPrefacePassed == false){
+                    //if you have just seen the video preface
+                    currentScenario.videoPrefacePassed = true;
+                    System.out.println("vp");
+                }else if (currentScenario.finalExplaination1Passed == false && currentScenario.choicePassed) {
+                    currentScenario.finalExplaination1Passed = true;
+                }else if (currentScenario.finalVideoPassed == false && currentScenario.finalExplaination1Passed) {
                     //if its time to show the video
                     showVideo = true;
-                } else if (currentScenario.initialExplainationPassed && currentScenario.choicePassed && currentScenario.finalVideoPassed) {
+                } else if (currentScenario.finalVideoPassed) {
                     //THEY HAVE FINISHED A SCENARIO
                     if (scenarioIndex + 1 < scenarioCount) {
                         //we still have scenarios so advance
@@ -239,40 +256,46 @@ public class LinkTrainer extends ApplicationAdapter implements InputProcessor {
         //the order is as follows: setup video, initial explanation, option 1, option 2, option 3, option 4, correct option NUMBER, action video, final explanation
         Scenario s1 = new Scenario( /*This is the handler upline cut*/
                 /*Setup video*/"video/uplineSetup.mp4",
-                /*Setup text*/ "In this scenario, you will be taking the place of the breakside handler on offense. Currently, your team is attempting to move the disc up the field which is to the left of your perspective. The person you see is the handler that has the disc. They were looking upfield but now that it is stall four, they have turned to you to get you to make a move. You will choose what is the best offensive move you can make. Press ENTER to continue.",
-                /*Option 1*/ "Option 1: Cut to the left and then come back for the disc. This would look like a v where you would be cutting straight up the field then stopping and coming towards the handler at an angle. Press 1 to select this answer.",
-                /*Option 2*/ "Option 2: Stay where you are and call for the disc. Trust that the handler with the disc will break the mark and that your defender will not be prepared to stop the break throw. Press 2 to select this answer.",
-                /*Option 3*/ "Option 3: Cut towards the handler with the disc for a few steps before shifting into an upline cut where you will move at a 45 degree angle to the handler trying to move across into the open space. Press 3 to select this answer.",
-                /*Option 4*/ "Option 4: Cut towards the handler with the disc for a few steps before shifting to come behind them to give them the easy dump option. Press 4 to select this answer.",
+                /*Setup text*/ "In this scenario, you will be taking the place of the breakside handler on offense. Currently, your team is attempting to move the disc up the field which is to the left of your perspective. The person you see is the handler that has the disc. They were looking upfield but now that it is stall four, they have turned to you to get you to make a move. You will choose what is the best offensive move you can make.",
+                /*Video Preface*/ "In the following video the player holding the disc is on the far sideline and is trying to move the disc up the field which is to the left of you screen. Watch the player in white wearing a black backwards hat.",
+                /*Option 1*/ "Option 1: Cut to the left and then come back for the disc. This would look like a v where you would be cutting straight up the field then stopping and coming towards the handler at an angle.",
+                /*Option 2*/ "Option 2: Stay where you are and call for the disc. Trust that the handler with the disc will break the mark and that your defender will not be prepared to stop the break throw.",
+                /*Option 3*/ "Option 3: Cut towards the handler with the disc for a few steps before shifting into an upline cut where you will move at a 45 degree angle to the handler trying to move across into the open space.",
+                /*Option 4*/ "Option 4: Cut towards the handler with the disc for a few steps before shifting to come behind them to give them the easy dump option.",
                 /*Correct option*/ 3,
                 /*Action video*/ "video/uplineAction.mp4",
-                /*Explanation text*/ "Correct! Option 3 is the best strategic decision. Since you are on the break side your cut will bring you into the open side, making it very easy for the handler with the disc to throw it to you. The 45 degree angle helps you lose your defender. Additionally, upline cuts put you in the power position. When you get the disc your defender will be behind you, you will have full view of the field, and your momentum will be going towards the endzone so you can easily throw a huck. Option 1 is incorrect because you would wind up clogging the cutting space and leaving only 2 handlers. Option 2 is incorrect because it may be a high stall or the handler with the disc may have problems getting around the mark. Finally, option 4 is not necessarily a bad choice but the upline cut in option 3 is better. Please press ENTER to watch the video of this being executed. When you have watched the video please press ENTER again to advance to the next scenario."
+                /*Explaination text (correct options)*/ "Correct! Option 3 is the best strategic decision. Since you are on the break side your cut will bring you into the open side, making it very easy for the handler with the disc to throw it to you. The 45 degree angle helps you lose your defender. Additionally, upline cuts put you in the power position. When you get the disc your defender will be behind you, you will have full view of the field, and your momentum will be going towards the endzone so you can easily throw a huck.",
+                /*Explaination text (incorrect options)*/ "Option 1 is incorrect because you would wind up clogging the cutting space and leaving only 2 handlers. Option 2 is incorrect because it may be a high stall or the handler with the disc may have problems getting around the mark. Finally, option 4 is not necessarily a bad choice but the upline cut in option 3 is better."
         );
         scenarios.add(s1);
 
         Scenario s2 = new Scenario( /*This is the sideline play*/
                 /*Setup video*/"video/sidelineSetup.mp4",
                 /*Setup text*/ "In this scenario, you are a cutter on the open-side sideline right in front of the disc. The disc is not coming in immediately, so you have a bit of time to set up. You have the handler with the disc in your vision, and the rest of the cutters are lined up in a horizontal stack. You now have the opportunity to choose your move. Press ENTER to continue.",
-                /*Option 1*/ "Option 1: Stay where you are and call for the disc, trusting the handler to give you a nice pass to space. Press 1 to select this answer.",
-                /*Option 2*/ "Option 2: Run straight deep and call for it - there is a lot of wide-open deep space. Press 2 to select this answer.",
-                /*Option 3*/ "Option 3: Run diagonally to the break side, and then cut hard back to the open side. Depending on the force, this would look like a greater-than sign (if the force is forehand) or a less-than sign (if the force is backhand). Press 3 to select this answer.",
-                /*Option 4*/ "Option 4: Run straight towards the handler for a few steps, and then run hard away from the sideline. This could act as either a cut to clear space or a break-side cut. Press 4 to select this answer.",
+                /*Video Preface*/ "In the following video the frisbee has just been turned over in the endzone. A handler is walking up to the endzone line with the frisbee. You will be following number 30 in white.",
+                /*Option 1*/ "Option 1: Stay where you are and call for the disc, trusting the handler to give you a nice pass to space.",
+                /*Option 2*/ "Option 2: Run straight deep and call for it - there is a lot of wide-open deep space.",
+                /*Option 3*/ "Option 3: Run diagonally to the break side, and then cut hard back to the open side. Depending on the force, this would look like a greater-than sign (if the force is forehand) or a less-than sign (if the force is backhand).",
+                /*Option 4*/ "Option 4: Run straight towards the handler for a few steps, and then run hard away from the sideline. This could act as either a cut to clear space or a break-side cut.",
                 /*Correct option*/ 4,
                 /*Action video*/ "video/sidelineAction.mp4",
-                /*Explanation text*/ "Great work! You correctly chose option 4. This cut is called an 'L-cut' and is very useful since it both clears space and offers the potential for the handler to break the mark and center the disc. Option 1 is not a good choice because you remain stagnant in the open space and clog it. Your handler also may not be able to throw something easy to space. Option 2 is not completely wrong, but still incorrect - the throw your handler would have to make to hit you running straight away from them is a very tough one. And, option 3 is sub-optimal because although you first clear out of the space, you re-enter it and end up clogging the open side. Even if you get the disc, it is still on the sideline - exactly where the defense wants you to be - and your defender likely is close behind you. Please press ENTER to watch the video of this being executed. When you have watched the video please press ENTER again to advance to the next scenario."
+                /*Explaination text (correct options)*/ "Great work! You correctly chose option 4. This cut is called an 'L-cut'. The L-cut is very useful since it both clears space and offers the potential for the handler to break the mark and center the disc. In the first case, the hanlder then has open space to look for a deep cut. If they break the mark and center it to you on your L-cut then you will have the whole open side to throw to.",
+                /*Explaination text (incorrect options)*/ "Option 1 is not a good choice because you remain stagnant in the open space and clog it. Your handler also may not be able to throw something easy to space. Option 2 is not completely wrong, but still incorrect - the throw your handler would have to make to hit you running straight away from them is a very tough one. And, option 3 is sub-optimal because although you first clear out of the space, you re-enter it and end up clogging the open side. Even if you get the disc, it is still on the sideline - exactly where the defense wants you to be - and your defender likely is close behind you."
         );
         scenarios.add(s2);
 
         Scenario s3 = new Scenario( /*This is the endzone play*/
                 /*Setup video*/"video/endzoneSetup.mp4",
                 /*Setup text*/ "Here, the disc has been turned over in the middle of the field on the endzone line. You have called for a vertical stack in the endzone, so four cutters are lined up behind you, and you can see the handler in the middle of the field about to pick up the disc. You now have the opportunity to choose your next move. Press ENTER to continue.",
+                /*Video Preface*/ "In the following video the disc has been turned over near the opposing team's goal line. Number 24 is our handler walking to pick up the disc. You will be taking the postion of the player in the front of the stack wearing a red and white hat.",
                 /*Option 1*/ "Option 1: Make a jab step, take three hard steps to the break side, and then quickly burst to the open side. If you pull this cut off, the handler will have a very easy time getting it to you. Press 1 to select this answer.",
                 /*Option 2*/ "Option 2: Cut directly towards the handler. Although you will drive into your defender, this could set you up for a continuation cut away from the handler. Press 2 to select this answer.",
                 /*Option 3*/ "Option 3: Make a few jab steps and then run hard to the open space on the break side. Trust that your handler will have the throws to get around their mark. Press 3 to select this answer.",
                 /*Option 4*/ "Option 4: Stay where you are. This gives others a chance to cut, and allows you to act as a continual anchor for the stack so that players do not get out of position. Press 4 to select this answer.",
                 /*Correct option*/ 1,
                 /*Action video*/ "video/endzoneAction.mp4",
-                /*Explanation text*/ "Nicely done! Option 1 is the best choice in this situation. The first step, making a jab step at your defender, could potentially get them on their heels – this is a very unbalanced position to be in. If so, they will overcommit to your second move: taking three hard steps to the break side. Even if they aren’t on their heels, the fact that they are so far behind you makes it very likely for your defender to run as hard as they can to catch up to you. In this stage, a defender is very vulnerable to your third move: quickly bursting to the open side. This progression of moves will result in a very high chance of you being open for an easy pass on the open side. Option 2 is incorrect because you end up clogging the throwing lane for your handler; you introduce two new bodies for them to deal with. Option 3 is incorrect because your handler may not be able to get a break-mark throw off. And, while option 4 is not entirely incorrect, the first option yields the best chance of scoring quickly."
+                /*Explaination text (correct options)*/ "Nicely done! Option 1 is the best choice in this situation. The first step, making a jab step at your defender, could potentially get them on their heels – this is a very unbalanced position to be in. If so, they will overcommit to your second move: taking three hard steps to the break side. Even if they aren’t on their heels, the fact that they are so far behind you makes it very likely for your defender to run as hard as they can to catch up to you. In this stage, a defender is very vulnerable to your third move: quickly bursting to the open side. This progression of moves will result in a very high chance of you being open for an easy pass on the open side.",
+                /*Explaination text (incorrect options)*/" Option 2 is incorrect because you end up clogging the throwing lane for your handler; you introduce two new bodies for them to deal with. Option 3 is incorrect because your handler may not be able to get a break-mark throw off. And, while option 4 is not entirely incorrect, the first option yields the best chance of scoring quickly."
         );
         scenarios.add(s3);
 
--- core/src/com/mygdx/game/Scenario.java
+++ core/src/com/mygdx/game/Scenario.java
@@ -13,31 +13,37 @@ public class Scenario {
 
     public boolean initialVideoPassed = false;
     public boolean initialExplainationPassed = false;
+    public boolean videoPrefacePassed = false;
     public boolean choicePassed = false;
     public boolean finalVideoPassed = false;
-    public boolean finalExplainationPassed = false;
+    public boolean finalExplaination1Passed = false;
+    public boolean finalExplaination2Passed = false;
 
     public String iv;
     public String ie;
+    public String vp;
     public String o1;
     public String o2;
     public String o3;
     public String o4;
     public int ca;
     public String fv;
-    public String fe;
+    public String fe1;
+    public String fe2;
     
     
-    public Scenario(String iv, String ie, String o1, String o2, String o3, String o4, int ca, String fv, String fe) {
+    public Scenario(String iv, String ie,String vp, String o1, String o2, String o3, String o4, int ca, String fv, String fe1, String fe2) {
         this.iv = iv;
         this.ie = ie;
+        this.vp = vp;
         this.o1 = o1;
         this.o2 = o2;
         this.o3 = o3;
         this.o4 = o4;
         this.ca = ca;
         this.fv = fv;
-        this.fe = fe;
+        this.fe1 = fe1;
+        this.fe2 = fe2;
     }
 
 }
