b91afb556c75ac5e4e6fbed590558ee7fc56bb04
elusch21 <eschreiber@eastsideprep.org>
2018-05-14 17:36:56 -0700

    Basic Setups and Animation Foundation
    look friends, I've finally committed something!

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -7,6 +7,10 @@ import com.mygdx.game.LinkTrainer;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "FrisbeeSim";
+		//config.resizable = false;
+		config.height = 480;
+		config.width = 640;
 		new LwjglApplication(new LinkTrainer(), config);
 	}
 }
